387d9de02001bd4aeec1745f5796518cb6cc1592
npp129 <n_pecora@me.com>
2022-06-06 20:55:03 +0200

    ref

--- src/Rhizosphere/wwwroot/scada.js
+++ src/Rhizosphere/wwwroot/scada.js
@@ -29,25 +29,24 @@ async function getStatus() {
 }
 
 async function drawStatus(status) {
-    drawElement("temperature",status.temperatureCelsius, "", " C°");
-    drawElement("humidity",status.humidityPercentage, "", " %");
+    drawElement("temperature", status.temperatureCelsius, "", " C°");
+    drawElement("humidity", status.humidityPercentage, "", " %");
 
 
-    drawElement("mode",status.mode, "", "");
+    drawElement("mode", status.mode, "", "");
 
-    drawElement("fanRunning",status.fanRunning, "", "");
-    drawElement("fogMachineRunning",status.fogMachineRunning, "", "");
+    drawElement("fanRunning", status.fanRunning, "", "");
+    drawElement("fogMachineRunning", status.fogMachineRunning, "", "");
 
 
-    drawElement("name",status.activeRecipe.name, "", "");
-    drawElement("commonName",status.activeRecipe.commonName, "", "");
-    drawElement("phaseName",status.activePhase.name, "", "");
+    drawElement("name", status.activeRecipe.name, "", "");
+    drawElement("commonName", status.activeRecipe.commonName, "", "");
+    drawElement("phaseName", status.activePhase.name, "", "");
 }
 
-function drawElement(id, status, prefix, suffix)
-{
+function drawElement(id, status, prefix, suffix) {
     let elem = document.getElementById(id);
 
-    if (id)
+    if (elem)
         id.textContent = prefix + status + suffix;
 }
